842e992b2ca303ce48b7f42bdd433cebdedbfaac
miqbalfr12 <miqbalfr12@gmail.com>
2024-08-28 07:55:01 +0700

    test commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const client = new Client({
    "--disable-accelerated-2d-canvas",
    "--no-first-run",
    "--no-zygote",
-   // '--single-process', // <- this one doesn't works in Windows
+   "--single-process", // <- this one doesn't works in Windows
    "--disable-gpu",
   ],
  },
