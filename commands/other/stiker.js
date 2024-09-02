b2ec7687bcd95af352af58fd8744c9c78744d296
miqbalfr12 <miqbalfr12@gmail.com>
2024-09-02 21:13:06 +0700

    feat stiker video gif

--- commands/other/stiker.js
+++ commands/other/stiker.js
@@ -1,52 +1,92 @@
 const fs = require("fs");
-const mime = require('mime-types');
-const { MessageMedia } = require("whatsapp-web.js");
+const mime = require("mime-types");
+const {execSync} = require("child_process");
+const {MessageMedia} = require("whatsapp-web.js");
 
 module.exports = {
-    name: "stiker",
-    aliases: ['s','stk'],
-    description: "stiker bot",
-    timeout: 0,
-    category: "other",
-    run: async(client, message) => {
-        const stiker = (pull) => {
-            pull.downloadMedia().then(media => {
-                if (media) {
-                    const mediaPath = './download/';
-                    if (!fs.existsSync(mediaPath)) {
-                        fs.mkdirSync(mediaPath);
-                    }
-                    const extension = mime.extension(media.mimetype);
-                    const filename = new Date().getTime();
-                    const fullFilename = mediaPath + filename + '.' + extension;
-                    // Save to file
-                    try {
-                        fs.writeFileSync(fullFilename, media.data, { encoding: 'base64' });
-                        console.log('File downloaded successfully!', fullFilename);
-                        console.log(fullFilename);
-                        MessageMedia.fromFilePath(filePath = fullFilename)
-                        client.sendMessage(pull.from, new MessageMedia(media.mimetype, media.data, filename), { sendMediaAsSticker: true,stickerAuthor:"Created By BaelzBot",stickerName:"BaelzBot"} )
-                        fs.unlinkSync(fullFilename)
-                        console.log(`File Deleted successfully!`,);
-                    } catch (err) {
-                        console.log('Failed to save the file:', err);
-                        console.log(`File Deleted successfully!`,);
-                    }
-                }
-            });
-        }
+ name: "stiker",
+ aliases: ["s", "stk"],
+ description: "stiker bot",
+ timeout: 0,
+ category: "other",
+ run: async (client, message) => {
+  const stiker = (pull) => {
+   pull.downloadMedia().then((media) => {
+    if (media) {
+     const mediaPath = "./download/";
+     if (!fs.existsSync(mediaPath)) {
+      fs.mkdirSync(mediaPath);
+     }
+     const extension = mime.extension(media.mimetype);
+     const filename = new Date().getTime();
+     const fullFilename = `${mediaPath}${filename}.${extension}`;
+     const webpFilename = ["jpg", "jpeg", "png", "webp"].includes(extension)
+      ? fullFilename
+      : `${mediaPath}${filename}.webp`;
 
-        if(message.hasMedia){
-            stiker(message)
-        } else if (message.hasQuotedMsg) {
-            const quotedMsg = await message.getQuotedMessage();
-            if (quotedMsg.hasMedia) {
-                stiker(quotedMsg)
-            } else {
-                quotedMsg.reply('ini gak bisa dibikin stiker broo, tag gambar dong.');
-            }
-        } else {
-            message.reply(`send image with caption *Stiker* `)
-        }
+     try {
+      fs.writeFileSync(fullFilename, media.data, {encoding: "base64"});
+      console.log("File downloaded successfully!", fullFilename);
+
+      if (extension === "mp4") {
+       execSync(
+        `ffmpeg -i ${fullFilename} -t 10 -vf "fps=10,scale='if(gt(iw,ih),320,-2)':'if(gt(ih,iw),320,-2)':force_original_aspect_ratio=decrease,pad=320:320:(ow-iw)/2:(oh-ih)/2:color=#00000000" -c:v libwebp -lossless 0 -q:v 90 ${webpFilename}`
+       );
+      } else if (extension === "gif") {
+       execSync(
+        `ffmpeg -i ${fullFilename} -vf "fps=10,scale='if(gt(iw,ih),320,-2)':'if(gt(ih,iw),320,-2)':force_original_aspect_ratio=decrease,pad=320:320:(ow-iw)/2:(oh-ih)/2:color=#00000000" -q:v 90 -c:v libwebp  -lossless 0 -q:v 90 ${webpFilename}`
+       );
+      } else if (
+       !["jpg", "jpeg", "png", "webp", "gif", "mp4"].includes(extension)
+      ) {
+       throw new Error("Unsupported file type for conversion");
+      }
+
+      // Send the WebP file as a sticker
+      const webpData = fs.readFileSync(webpFilename, {
+       encoding: "base64",
+      });
+      //   MessageMedia.fromFilePath(webpFilename);
+      const sticker = new MessageMedia(
+       `image/${
+        ["jpg", "jpeg", "png", "webp"].includes(extension) ? extension : "webp"
+       }`,
+       webpData,
+       filename
+      );
+      
+      client.sendMessage(pull.from, sticker, {
+       sendMediaAsSticker: true,
+       stickerAuthor: "Created By BaelzBot",
+       stickerName: "BaelzBot",
+      });
+
+      // Cleanup
+      fs.unlinkSync(fullFilename);
+      if (!["jpg", "jpeg", "png", "webp"].includes(extension))
+       fs.unlinkSync(webpFilename);
+      console.log("Files deleted successfully!");
+     } catch (err) {
+      console.log("Failed to process the file:", err);
+     }
     }
-}
+   });
+  };
+
+  if (message.hasMedia) {
+   stiker(message);
+  } else if (message.hasQuotedMsg) {
+   const quotedMsg = await message.getQuotedMessage();
+   console.log(quotedMsg);
+   if (quotedMsg.hasMedia) {
+    stiker(quotedMsg);
+   } else {
+    quotedMsg.reply(
+     "ini gak bisa dibikin stiker broo, tag gambar dong.\n\n_BaelzBot_"
+    );
+   }
+  } else {
+   message.reply(`send image with caption *Stiker* \n\n_BaelzBot_`);
+  }
+ },
+};
