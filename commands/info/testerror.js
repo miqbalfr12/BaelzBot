e44f4ec37f82b381164a09f1d743610d1b316b5f
miqbalfr12 <miqbalfr12@gmail.com>
2024-09-19 16:33:59 +0700

    testerror

--- commands/info/testerror.js
+++ commands/info/testerror.js
@@ -1,10 +1,11 @@
 module.exports = {
- name: "errortest",
+ name: "testerror",
  aliases: ["et"],
  description: "Test Error",
  timeout: 5000,
- category: "errortest",
+ category: "testerror",
  run: (client, message) => {
+  console.log("Test Error");
   throw new Error("Test Error");
  },
 };
